f38706ca8dbe3056743bbc3b4367a7f665a55665
Warren Lim <85139891+wrrnlim@users.noreply.github.com>
2023-02-07 22:30:04 -0700

    successful GET requests

--- httpclient.py
+++ httpclient.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # coding: utf-8
 # Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,16 +24,19 @@ import re
 # you may use urllib to encode data appropriately
 import urllib.parse
 
+
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
+
 
 class HTTPResponse(object):
     def __init__(self, code=200, body=""):
         self.code = code
         self.body = body
 
+
 class HTTPClient(object):
-    #def get_host_port(self,url):
+    # def get_host_port(self,url):
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -41,24 +44,27 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        headers = self.get_headers(data)
+        code = headers[0].split(" ")
+        return ' '.join(x for x in code[1:]) # code number + description
 
-    def get_headers(self,data):
-        return None
+    def get_headers(self, data):
+        data = data.splitlines()
+        return data[:data.index("")]
 
     def get_body(self, data):
-        return None
-    
+        data = data.splitlines()
+        data = data[data.index(""):]
+        return ''.join(x for x in data)
+
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
-        print(self.socket)
-        
+
     def close(self):
         self.socket.close()
 
     # read everything from the socket
     def recvall(self, sock):
-        print(sock)
         buffer = bytearray()
         done = False
         while not done:
@@ -67,15 +73,15 @@ class HTTPClient(object):
                 buffer.extend(part)
             else:
                 done = not part
+        print(buffer)
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
 
         path = url.split("/", 1)
         host = path[0]
-        self.connect(host, 8080)
+        port = 80
+        self.connect(host, port)
 
         if len(path) > 1:
             path = path[1]
@@ -87,10 +93,14 @@ class HTTPClient(object):
 
         self.sendall(request)
 
+        self.socket.shutdown(socket.SHUT_WR)
+
         response = self.recvall(self.socket)
         self.close()
-        print(response)
-
+        
+        code = self.get_code(response)
+        body = self.get_body(response)
+        print(code)
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
